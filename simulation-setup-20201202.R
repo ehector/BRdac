4bee6b2cea02d8c88e5409156a5e44467df15c5a
ehector <ehector@ncsu.edu>
2020-12-03 16:51:59 -0500

    Add files via upload

--- simulation-setup-20201202.R
+++ simulation-setup-20201202.R
@@ -5,34 +5,34 @@ library(Rcpp)
 sourceCpp("/Users/ehector/Dropbox/Projects/SpatialExtremes-Brian/simulations/CL-funcs-20201202.cpp")
 source("/Users/ehector/Dropbox/Projects/SpatialExtremes-Brian/simulations/R-wrappers-20201202.R")
 
-alpha <- 0.5
+alpha <- 1
 phi <- 3
-N <- 7
-s <- as.matrix(expand.grid(1:3,1:3))
+N <- 100
+s <- as.matrix(expand.grid(1:10,1:10))
 S <- nrow(s)
 p_1 <- p_2 <- p_3 <- 2
 beta_1 <- c(0.5,0.5)
 beta_2 <- c(1,1)
-beta_3 <- c(0.5,0.5)
+beta_3 <- c(2,2)
 
-mod <- RPbrownresnick(xi=1, mu=1, s=1, phi=RMfbm(alpha=alpha, scale=phi))
-invisible(capture.output(R <- RFsimulate(mod, n=N, x=s[,1], y=s[,2])))
-X <- t(as.matrix(R@data))
+set.seed(1234)
+X <- rmaxstab(n=N, coord=s, cov.mod="brown", grid=FALSE, range=phi, smooth=alpha)
 
+#suppressWarnings( init_values <- fitmaxstab(X, s, cov.mod="brown", fit.marge=FALSE)$fitted.values )
 opt <- nlminb(start=c(1,1), objective=logCL_BR_wrap, gradient=Cscore_BR_sum_wrap, x=X, locs=s, 
              lower=c(1e-5,1e-5), upper=c(2-1e-5,Inf))
 psi <- do.call(cbind, Cscore_BR(opt$par[1], opt$par[2], x=X, locs=s))
 
-covariates <- do.call(rbind, lapply(1:N, function(x) return(s))) #matrix(rnorm(N*S*p, 0, 1), N*S, p)
+#covariates <- do.call(rbind, lapply(1:N, function(x) return(s))) 
+covariates <- do.call(rbind, lapply(1:N, function(x) return(s))) /10
 z_1 <- z_2 <- z_3 <- z_constructor(covariates, S, N, p_1)
 mu <- apply(z_1, 1, function(x) x%*%beta_1)
 sigma <- apply(z_2, 1, function(x) exp(x%*%beta_2))
 xi <- apply(z_3, 1, function(x) x%*%beta_3)
-
 Y <- mu + sigma/xi*(X^xi - 1)
 
 opt_2 <-
-  nlminb(start=c(alpha, phi,beta_1,beta_2,beta_3), objective=logCL_all_wrap, gradient=Cscore_all_sum_wrap, 
+  nlminb(start=c(opt$par, rep(1,p_1+p_2+p_3)), objective=logCL_all_wrap, gradient=Cscore_all_sum_wrap, 
          y=Y, z_1=z_1, z_2=z_2, z_3=z_3, locs=s,
          lower=c(1e-5,1e-5,rep(-Inf,p_1+p_2+p_3)), upper=c(2-1e-5,rep(Inf,1+p_1+p_2+p_3)))
 
